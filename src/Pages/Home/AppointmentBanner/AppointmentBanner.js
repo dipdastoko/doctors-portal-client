d499b45f6aec5f3491457d470836c8ed6ac4e8c2
dipdastoko <dipdastoko@gmail.com>
2022-08-08 02:10:47 +0600

    home appointment banner added

--- src/Pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -3,23 +3,41 @@ import Box from '@mui/system/Box';
 import Grid from '@mui/system/Unstable_Grid';
 import doctor from '../../../images/doctor.png';
 import bg from '../../../images/appointment-bg.png';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
-const appointmentBg = {
-    background: `url(${bg})`
+const appointmentBanner = {
+    background: `url(${bg})`,
+    backgroundColor: 'rgba(45, 58, 74, 0.9)',
+    backgroundBlendMode: 'darken, luminosity',
+    marginTop: 150
+
 }
 
 const AppointmentBanner = () => {
     return (
-        <Box style={appointmentBg} sx={{ flexGrow: 1 }}>
+        <Box style={appointmentBanner} sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid xs={12} md={6}>
-                    <img style={{ width: '400px' }} src={doctor} alt="" />
+                    <img style={{ width: '400px', marginTop: '-110px' }} src={doctor} alt="" />
                 </Grid>
-                <Grid xs={12} md={6}>
-                    <Typography variant='h6'>
-                        Appointment
-                    </Typography>
+                <Grid xs={12} md={6} sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-start',
+                    textAlign: 'left',
+                    alignItems: 'center'
+                }}>
+                    <Box>
+                        <Typography sx={{ mb: 5 }} style={{ color: '#42DD53' }} variant='h6'>
+                            Appointment
+                        </Typography>
+                        <Typography style={{ color: 'white' }} variant='h4'>
+                            Make an Appointment Today
+                        </Typography>
+                        <Typography sx={{ my: 5 }} style={{ color: 'white', fontSize: 14, fontWeight: 300 }} variant='h6'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur pariatur quibusdam atque ducimus adipisci. Magni est consequatur itaque quo rem?
+                        </Typography>
+                        <Button variant='contained'>Learn More</Button>
+                    </Box>
                 </Grid>
             </Grid>
         </Box>
