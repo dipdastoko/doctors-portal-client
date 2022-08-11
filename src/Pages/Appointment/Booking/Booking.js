32e15083c8aab981b805b52d1a20add18ad174a2
dipdastoko <dipdastoko@gmail.com>
2022-08-12 03:48:13 +0600

    Booking Modal Added

---
+++ src/Pages/Appointment/Booking/Booking.js
@@ -0,0 +1,37 @@
+import { Button, Grid, Paper, Typography } from '@mui/material';
+import React from 'react';
+import BookingModal from '../BookingModal/BookingModal';
+
+const Booking = ({ booking, date }) => {
+    const { name, space, time } = booking;
+    const [openBooking, setOpenBooking] = React.useState(false);
+    const handleBookingOpen = () => setOpenBooking(true);
+    const handleBookingClose = () => setOpenBooking(false);
+    return (
+
+        <>
+            <Grid item xs={12} sm={6} md={4}>
+                <Paper sx={{ py: 5 }} elevation={3}>
+                    <Typography sx={{ color: 'info.main', fontWeight: 600 }} variant='h5'>
+                        {name}
+                    </Typography>
+                    <Typography variant='h6'>
+                        {time}
+                    </Typography>
+                    <Typography variant='caption'>
+                        {space} Spaces Available
+                    </Typography>
+                    <br />
+                    <Button onClick={handleBookingOpen} variant='contained'>Book Appointment</Button>
+                </Paper>
+            </Grid>
+            <BookingModal
+                date={date}
+                booking={booking}
+                openBooking={openBooking}
+                handleBookingClose={handleBookingClose}></BookingModal>
+        </>
+    );
+};
+
+export default Booking;
