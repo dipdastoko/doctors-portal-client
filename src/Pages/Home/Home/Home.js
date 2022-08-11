d5075fcbb81df457ee2dd32eebac02eb5a9c6dbf
dipdastoko <dipdastoko@gmail.com>
2022-08-11 12:09:57 +0600

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 import Navigation from '../Shared/Navigation/Navigation';
 
@@ -7,6 +8,7 @@ const Home = () => {
     return (
         <div>
             <Navigation />
+            <Banner />
             <Services />
             <AppointmentBanner />
         </div>
