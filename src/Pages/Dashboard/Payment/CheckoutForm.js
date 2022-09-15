9537300c0a06f9faa4d16e0643a20b8629398197
dipdastoko <dipdastoko@gmail.com>
2022-09-15 11:17:14 +0600

    checkoutform for payment

---
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -0,0 +1,120 @@
+import { CircularProgress } from '@mui/material';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import useAuth from '../../../Hooks/useAuth';
+
+const CheckoutForm = ({ appointment }) => {
+    const { price, patientName } = appointment;
+    const stripe = useStripe();
+    const elements = useElements();
+    const { user } = useAuth();
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [clientSecret, setClientSecret] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => setClientSecret(data.clientSecret));
+
+    }, [price]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        };
+
+        const card = elements.getElement(CardElement);
+
+        if (card === null) {
+            return;
+        };
+        setProcessing(true);
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+        if (error) {
+            setError(error.message);
+            setSuccess('');
+            setProcessing(false);
+        }
+        else {
+            setError('');
+            console.log(paymentMethod);
+            setProcessing(false);
+        };
+
+        // payment intent
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientName,
+                        email: user.email
+                    },
+                },
+            },
+        );
+
+        if (intentError) {
+            setError(intentError.message);
+            setSuccess('');
+            setProcessing(false);
+        }
+        else {
+            setError('');
+            setSuccess('Your payment processed successfully');
+            console.log(paymentIntent);
+            setProcessing(false);
+        }
+
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                {processing ? <CircularProgress /> : <button type="submit" disabled={!stripe}>
+                    Pay ${price}
+                </button>}
+            </form>
+            {
+                error && <p style={{ color: 'red' }}>{error}</p>
+            }
+            {
+                success && <p style={{ color: 'green' }}>{success}</p>
+            }
+        </div>
+    );
+};
+
+export default CheckoutForm;
--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,10 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51LgI42Cg6MihJhLWJ9ZYJcDJnvesCigAKHQU75joOvbZX8lKqe1VkBqbtUvWucAZeIK82lf10blZuKougPJ4oPaN00pNFWJ8pO');
 
 const Payment = () => {
     const { appointmentId } = useParams();
@@ -13,8 +18,25 @@ const Payment = () => {
         <div>
             <h2>Please Pay for: {appointment.patientName} for {appointment.serviceName}</h2>
             <h4>Pay: ${appointment.price}</h4>
+
+            {
+                appointment?.price && <Elements stripe={stripePromise}>
+                    <CheckoutForm
+                        appointment={appointment}
+                    />
+                </Elements>
+            }
         </div>
     );
 };
 
 export default Payment;
+
+/* 
+1. install stripe and stripe-react
+2. set publishable key
+3. Elements
+4. Checkout Form
+-------
+5. Create payment method
+*/
